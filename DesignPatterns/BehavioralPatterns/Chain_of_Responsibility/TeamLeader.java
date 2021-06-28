bf9d930b468f254c4590e283897ce19d1b115896
Sathwik Reddy <sathwik.kotla@zemosolabs.com>
2021-06-28 11:55:22 +0530

    First commit

---
+++ DesignPatterns/BehavioralPatterns/Chain_of_Responsibility/TeamLeader.java
@@ -0,0 +1,2 @@
+package com.company.DesignPatterns.BehavioralPatterns.Chain_of_Responsibility;public class TeamLeader {
+}
---
+++ DesignPatterns/BehavioralPatterns/Mediator_Pattern/UserImpl.java
@@ -0,0 +1,2 @@
+package com.company.DesignPatterns.BehavioralPatterns.Mediator_Pattern;public class UserImpl {
+}
